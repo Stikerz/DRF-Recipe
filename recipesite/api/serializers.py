4acdb4b90d6bfc9f16c98dacaa881382465a0cba
Stikerz <abiola_saraki@live.co.uk>
2019-04-30 13:55:24 +0100

    create serializers

---
+++ recipesite/api/serializers.py
@@ -0,0 +1,93 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Step, Ingredient, Recipe
+
+from django.contrib.auth import get_user_model
+
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['username']
+
+class IngredientSerializer(ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ('text',)
+
+
+class StepSerializer(ModelSerializer):
+    class Meta:
+        model = Step
+        fields = ('step_text',)
+
+
+class RecipeSerializer(ModelSerializer):
+    recipe_ingredients = IngredientSerializer(many=True)
+    recipe_steps = StepSerializer(many=True)
+
+    class Meta:
+        model = Recipe
+        fields = ('name', 'recipe_ingredients', 'recipe_steps')
+
+    def create(self, validated_data):
+        recipe_ingredients: list = validated_data.pop('recipe_ingredients')
+        recipe_steps: list = validated_data.pop('recipe_steps')
+        user = validated_data.pop('user')
+        recipe_instance, created = Recipe.objects.get_or_create(
+            user=user,
+                                                **validated_data)
+        for ingrediants in recipe_ingredients:
+            Ingredient.objects.create(recipe=recipe_instance, **ingrediants)
+        for step in recipe_steps:
+            Step.objects.create(recipe=recipe_instance, **step)
+        return recipe_instance
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data['name']
+        recipe_ingredients = validated_data['recipe_ingredients']
+        recipe_steps = validated_data['recipe_steps']
+        instance.save()
+        ingredient = instance.recipe_ingredients.all()
+        ingredient.delete()
+        instruction = instance.recipe_steps.all()
+        instruction.delete()
+        for ingrediants in recipe_ingredients:
+            Ingredient.objects.get_or_create(recipe=instance, **ingrediants)
+        for step in recipe_steps:
+            Step.objects.create(recipe=instance, **step)
+        return instance
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# {
+#     "user": {
+#         "username": "admin"
+#     },
+#     "name": "dfdf",
+#     "recipe_ingredients": [{
+#                 "text": "Chicken"
+#             },
+#             {
+#                 "text": "Flour"
+#             }],
+#     "recipe_steps": [{
+#                 "step_text": "clean chicken"
+#             },
+#             {
+#                 "step_text": "fry chicken"
+#             }]
+# }
